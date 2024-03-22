78cffffd67337385415648fccb2aa031d22aa9f9
Uziel Tzab <uzieltzab8@gmail.com>
2024-03-22 13:55:07 -0600

    QUitado de cors exitoso el anterior no

--- ApiType/src/index.ts
+++ ApiType/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
+
 import { createConnection } from 'mysql';
 import userRoutes from './routes/UserRoutes';
 import floreriaRoutes from './routes/FloreriaRoutes';
@@ -27,7 +27,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+
 
 // Middleware para manejar datos JSON
 app.use(express.json());
